cee3c2202d15907ab1d9fc33c6e00f8869b352e4
Harsh Rai <harsh.rai.eee21@itbhu.ac.in>
2023-06-06 15:13:28 +0530

    Day2

---
+++ cycle_detection1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+bool detectCycle(Node *head)
+{
+    //	Write your code here
+    if (head == NULL || head->next == NULL)
+        return false;
+    Node *fast = head;
+    Node *slow = head;
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
